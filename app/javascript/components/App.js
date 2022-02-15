fa004db274cc4c5a3b0a5034aae7ef38a08e5b02
kirkkwang <k3wang@gmail.com>
2022-02-15 15:27:27 -0800

    initial commit

---
+++ app/javascript/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <h1>Hello, Ruby On Ramen!</h1>
+      </>
+    );
+  }
+}
