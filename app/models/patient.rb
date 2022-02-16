a31d3da3a3181b3ff4051d64eb9648cb5b5b6075
kirkkwang <k3wang@gmail.com>
2022-02-16 13:11:20 -0800

    completed patient model validations

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,4 +3,12 @@
 class Patient < ApplicationRecord
   has_many :medications
   validates :first_name, :last_name, :dob, :gender, :image, presence: true
+  validates :first_name,
+            :last_name,
+            :gender,
+            :image,
+            format: {
+              with: /\A[a-zA-Z]+\z/,
+              message: 'Only letters allowed',
+            }
 end
