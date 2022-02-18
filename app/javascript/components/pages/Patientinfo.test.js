832cc104a77410a41556b6820b6811ce75933a60
Samanthap803b <samanthap803b@gmail.com>
2022-02-18 10:09:57 -0800

    added test for home.js ans patientinfo.js

--- app/javascript/components/pages/Patientinfo.test.js
+++ app/javascript/components/pages/Patientinfo.test.js
@@ -2,16 +2,17 @@ import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import PatientInfo from "./PatientInfo";
+import fetch from "node-fetch"
 
 Enzyme.configure({ adapter: new Adapter() });
 
-// describe("", () => {
-//   it("", () => {
-//     const home = shallow(<Home />);
-//     const homeHeading = home.find("h1").text();
-//     expect(homeHeading).toEqual("Patients");
-//   });
-// });
+describe("when Patientinfo  renders", () => {
+  it("display patient first name ", () => {
+    const home = shallow(<PatientInfo  />);
+    const homeHeading = home.find("h2").text();
+     expect(homeHeading).toEqual("Stefani");
+  });
+});
 
 // img file
 // first_name
