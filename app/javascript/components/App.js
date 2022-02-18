426fa8a4e6e92646ac2f7299f9702dfe70f3fda9
kirkkwang <k3wang@gmail.com>
2022-02-17 17:36:20 -0800

    finished read, just needs testing

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,23 +1,67 @@
 import React, { Component } from "react";
-import PatientIndex from "./pages/PatientIndex";
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Route,
+  Switch,
+} from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import PatientInfo from "./pages/PatientInfo";
 import "./App.css";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      patients: [],
-      meds: [],
+      patientsArray: [],
     };
-    fetch("/patients/2")
-      .then((resp) => resp.json())
-      .then((patient) => this.setState({ meds: patient.medications }));
   }
+
+  componentDidMount() {
+    this.readPatients();
+  }
+
+  readPatients = () => {
+    fetch("/patients")
+      .then((resp) => resp.json())
+      .then((patients) => this.setState({ patientsArray: patients }))
+      .catch((errors) => console.log("Patients errors:", errors));
+  };
+
   render() {
+    const {
+      logged_in,
+      current_user,
+      new_user_route,
+      sign_in_route,
+      sign_out_route,
+    } = this.props;
     return (
-      <div className="app">
-        <PatientIndex meds={this.state.meds} />
-      </div>
+      <>
+        <Router>
+          {/* <Header {...this.props} /> */}
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={() => <Home patientsArray={this.state.patientsArray} />}
+            />
+            <Route
+              path="/patientinfo/:id"
+              render={(props) => {
+                let id = +props.match.params.id;
+                console.log(id);
+                let patientInfo = this.state.patientsArray.find(
+                  (patient) => patient.id === id
+                );
+                return <PatientInfo patientInfo={patientInfo} id={id} />;
+              }}
+            />
+          </Switch>
+          {/* <Footer /> */}
+        </Router>
+      </>
     );
   }
 }
---
+++ app/javascript/components/pages/Home.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+
+export default class Home extends Component {
+  render() {
+    const { patientsArray } = this.props;
+    return (
+      <>
+        <h1>Patients</h1>
+        {patientsArray.map((patient) => {
+          return (
+            <NavLink to={`/patientinfo/${patient.id}`} key={patient.id}>
+              <img
+                src={patient.image}
+                alt={patient.first_name}
+                style={{ width: "200px" }}
+              />
+              <p>{patient.first_name}</p>
+              <p>{patient.last_name}</p>
+            </NavLink>
+          );
+        })}
+      </>
+    );
+  }
+}
