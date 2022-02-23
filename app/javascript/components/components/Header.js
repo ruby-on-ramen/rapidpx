acaa9784733a4f808b045599da4fdbd6aa4b053b
Kirk Wang <kirkkwang@kirks-mbp.lan>
2022-02-23 06:58:01 -0800

    added styles for sign up and forgot password devise pages

--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -1,32 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import { NavLink, Link } from "react-router-dom";
 import "../../../assets/stylesheets/Header.css";
-const Header = () => {
-  const {
-    logged_in,
-    current_user,
-    new_user_route,
-    sign_in_route,
-    sign_out_route,
-  } = this.props;
-  return (
-    <nav>
-      <div className="logo">
-        RapidP<span>x</span>
-      </div>
-      <div>
-        <NavLink to="/about" className="nav-link">
-          About Us
-        </NavLink>
-        <NavLink to="/contact" className="nav-link">
-          Contact Us
-        </NavLink>
-        <NavLink to="/signout" className="nav-link">
-          Sign Out
-        </NavLink>
-      </div>
-    </nav>
-  );
-};
 
-export default Header;
+export default class Header extends Component {
+  render() {
+    const {
+      logged_in,
+      current_user,
+      new_user_route,
+      sign_in_route,
+      sign_out_route,
+    } = this.props;
+    return (
+      <nav>
+        <div className="logo">
+          RapidP<span>x</span>
+        </div>
+        <div>
+          <NavLink to="/about" className="nav-link">
+            About Us
+          </NavLink>
+          <NavLink to="/contact" className="nav-link">
+            Contact Us
+          </NavLink>
+          <a href={sign_out_route} className="nav-link">
+            Sign Out
+          </a>
+        </div>
+      </nav>
+    );
+  }
+}
