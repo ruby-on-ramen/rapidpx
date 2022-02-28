f733b6bf95dd79a8c8260da61b18a0a6333f52a2
Kirk Wang <kirkkwang@kirks-mbp.lan>
2022-02-28 13:59:01 -0800

    deployment attempt

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,8 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+process.env.NODE_ENV = process.env.NODE_ENV || "production";
 
-const environment = require('./environment')
-
-module.exports = environment.toWebpackConfig()
+const environment = require("./environment");
+environment.loaders.get("babel").exclude = [
+  /\.test\.js$/,
+  environment.loaders.get("babel").exclude,
+];
+module.exports = environment.toWebpackConfig();
